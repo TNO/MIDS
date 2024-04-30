7412add4019e6c0a598c1ee8d5dfcf506f79dcaa
Dennis Hendriks <dhendriks@users.noreply.github.com>
2024-04-30 17:51:09 +0200

    Update copyright year to 2024.

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ For developers:
 
 ## License
 
-Copyright (c) 2018-2023 TNO and Contributors to the GitHub community
+Copyright (c) 2018-2024 TNO and Contributors to the GitHub community
 
 This program and the accompanying materials are made available under the terms of the [MIT License](LICENSE).
 
--- plugins/nl.tno.mids.compare.test/test/copy_actual_to_expected.bash
+++ plugins/nl.tno.mids.compare.test/test/copy_actual_to_expected.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #########################################################################
-# Copyright (c) 2018-2023 TNO and Contributors to the GitHub community
+# Copyright (c) 2018-2024 TNO and Contributors to the GitHub community
 #
 # This program and the accompanying materials are made available
 # under the terms of the MIT License which is available at
